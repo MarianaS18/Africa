ecf631de1ea00f589f6c79f2d3abf15a9d0a6078
Mariana <mariana@fastmail.net>
2021-04-10 15:51:01 +0200

    create decode method to local json files

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension Bundle {
+    
+    // decode the local JSON file
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the json file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Fail to locate \(file) in bundle")
+        }
+        
+        // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Fail to load \(file) from bundle")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Fail to decode \(file) from bundle")
+        }
+        
+        // 5. Return the ready-to-use data
+        return loaded
+    }
+}
