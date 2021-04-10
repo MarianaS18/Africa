041d5f10a8f89d46e1881392e5f6f47382e0a8ac
Mariana <mariana@fastmail.net>
2021-04-10 16:18:56 +0200

    create generic mothod to decode json objects

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Bundle {
     
     // decode the local JSON file
-    func decode(_ file: String) -> [CoverImage] {
+    func decodeOneType(_ file: String) -> [CoverImage] {
         // 1. Locate the json file
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Fail to locate \(file) in bundle")
@@ -26,6 +26,28 @@ extension Bundle {
         
         // 4. Create a property for the decoded data
         guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Fail to decode \(file) from bundle")
+        }
+        
+        // 5. Return the ready-to-use data
+        return loaded
+    }
+    
+    
+    
+    // generic moethod
+    func decode<T: Codable>(_ file: String) -> [T] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Fail to locate \(file) in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Fail to load \(file) from bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([T].self, from: data) else {
             fatalError("Fail to decode \(file) from bundle")
         }
         
