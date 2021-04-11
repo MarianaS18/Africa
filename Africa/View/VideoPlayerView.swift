c3b6515ec762b79bbe045251c1df405b3335d9d4
Mariana <mariana@fastmail.net>
2021-04-11 20:00:48 +0200

    create a view with video player

--- Africa/View/VideoPlayerView.swift
+++ Africa/View/VideoPlayerView.swift
@@ -6,15 +6,44 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    let videoSelected: String
+    let videoTitle: String
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+            } //: VIDEO PLAYER
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8)
+                , alignment: .topLeading
+            )
+        } //: VSTACK
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
     }
 }
 
+    // MARK: - PREVIEW
+
 struct VideoPlayerView_Previews: PreviewProvider {
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    
     static var previews: some View {
-        VideoPlayerView()
+        NavigationView {
+            VideoPlayerView(videoSelected: videos[0].id, videoTitle: videos[0].name)
+        }
+            
     }
 }
