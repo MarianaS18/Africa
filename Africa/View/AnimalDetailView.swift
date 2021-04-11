9f25ea82fa4cf896c6da6d12ababac6ec1547cc9
Mariana <mariana@fastmail.net>
2021-04-11 09:02:52 +0200

    create detail view to an animal

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -8,13 +8,71 @@
 import SwiftUI
 
 struct AnimalDetailView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // Hero image
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // Title
+                Text(animal.name.uppercased())
+                    .font((.largeTitle))
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // Headline
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                // Gallery
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in pictures")
+                    
+                    InsetGallaryView(animal: animal)
+                } //: GROUP
+                .padding(.horizontal)
+                
+                // Facts
+                
+                // Description
+                
+                // Map
+                
+                // Link
+                
+            } //: VSTACK
+                    .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        } //: SCROLL
     }
 }
+    
+    // MARK: - PREVIEW
 
 struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     static var previews: some View {
-        AnimalDetailView()
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+        .previewDevice("iPhone 12 mini")
     }
 }
