2c145cacf4f75a16a96129cdce43a9cff5055e36
Mariana <mariana@fastmail.net>
2021-04-11 10:17:27 +0200

    create a reusable map view to show the location of an animal

---
+++ Africa/View/InsetFactView.swift
@@ -0,0 +1,20 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Mariana Steblii on 11/04/2021.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InsetFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetFactView()
+    }
+}
