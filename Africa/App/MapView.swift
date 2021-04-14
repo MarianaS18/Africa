ddebbdbb8c115a9763bf60d1302e7b318f4043e8
Mariana <mariana@fastmail.net>
2021-04-14 18:47:22 +0200

    add map and mark to the map view

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,12 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    // we need to use @state because map uses binding to track any changes  such moving or zooming
+    @State private var region: MKCoordinateRegion = {
+        // coordinates of the map
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        // zoom level of the map
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        // actual map region with the combination of the coordinates of the map and zoom level
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Map")
+        // MARK: - NO1 BASIC MAP
+        // Map(coordinateRegion: $region)
+        
+        // MARK: - NO2 ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE (always static)
+            // MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: NEW STYLE (always static)
+            // MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            } //: ANNOTATION
+        })
+        
     }
 }
+
+    // MARK: - PREVIEW
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
