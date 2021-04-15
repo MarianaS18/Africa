4a844c104e0a4ff483042cee2aeb6ae11deabbbe
Mariana <mariana@fastmail.net>
2021-04-15 15:19:52 +0200

    create a new modifier

--- Africa/Modifier/CenterModifier.swift
+++ Africa/Modifier/CenterModifier.swift
@@ -5,4 +5,14 @@
 //  Created by Mariana Steblii on 15/04/2021.
 //
 
-import Foundation
+import SwiftUI
+
+struct CenterModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        HStack {
+            Spacer()
+            content
+            Spacer()
+        }
+    }
+}
